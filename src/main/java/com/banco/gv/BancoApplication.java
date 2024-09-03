ebe6051e1b2a9ad0f88df1ee3488f98290d8897d
giovic16 <giovanavictoria887@gmail.com>
2024-09-03 18:32:32 -0300

    first commit

---
+++ src/main/java/com/banco/gv/BancoApplication.java
@@ -0,0 +1,14 @@
+package com.banco.gv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/banco/gv/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.gv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
